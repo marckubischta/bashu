f7577fd25fef2520ce7931bc1e38c46d4e5511a2
Marc Kubischta <mkubisch@adobe.com>
2016-02-29 10:21:36 -0800

    fix osx dns cache flush alias

--- osx.sh
+++ osx.sh
@@ -1,10 +1,16 @@
 # osx finder
 alias showall="defaults write com.apple.finder AppleShowAllFiles TRUE; killall Finder"
 alias unshowall="defaults write com.apple.finder AppleShowAllFiles FALSE; killall Finder"
+
 alias dq="xattr -d com.apple.quarantine"
+
 alias dockdimhide="defaults write com.apple.Dock showhidden -bool YES; killall Dock"
 alias docknoglass="defaults write com.apple.dock no-glass -boolean YES; killall Dock"
 alias dockinstahide="defaults write com.apple.Dock autohide-delay -float 0.05; defaults write com.apple.dock autohide-time-modifier -float 0.05; killall Dock"
+
 alias ccscreenshots="defaults write com.apple.screencapture location ~/Creative\ Cloud\ Files/Screenshots/;killall SystemUIServer"
 alias screenshots="defaults write com.apple.screencapture location ~/Screenshots/;killall SystemUIServer"
-alias flushdns="dscacheutil -flushcache; sudo discoveryutil mdnsflushcache; sudo discoveryutil udnsflushcaches"
+
+alias flushdns="test \"`which dscacheutil`\" != \"\" && sudo dscacheutil -flushcache; \
+                test \"`which discoveryutil`\" != \"\" && sudo discoveryutil mdnsflushcache && sudo discoveryutil udnsflushcaches; \
+                test \"`which mDNSResponder`\" != \"\" && sudo killall -HUP mDNSResponder"
