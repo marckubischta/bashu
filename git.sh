8596de3a9426c42db223437b85129a273e0c6eba
Marc Kubischta <mkubisch@adobe.com>
2016-03-18 12:37:02 -0700

    Add references for git completion / git status.

--- git.sh
+++ git.sh
@@ -1,4 +1,6 @@
 
+# git completion / shell status
+# from https://github.com/git/git/tree/master/contrib/completion
 source $DROPBOX_SHELL/scripts/git-completion.bash
 source $DROPBOX_SHELL/scripts/git-prompt.sh
 export GIT_PS1_SHOWDIRTYSTATE=1
@@ -28,30 +30,6 @@ alias gl="git log --pretty=oneline"
 alias gr="git remote -v && git branch -a --list --verbose"
 alias gd="git diff"
 alias gb="git branch"
-function _git_prompt() {
-    local git_status="`git status -unormal 2>&1`"
-    if ! [[ "$git_status" =~ Not\ a\ git\ repo ]]; then
-        if [[ "$git_status" =~ nothing\ to\ commit ]]; then
-            local ansi=32
-        elif [[ "$git_status" =~ nothing\ added\ to\ commit\ but\ untracked\ files\ present ]]; then
-            local ansi=33
-        else
-            local ansi=31
-        fi
-        if [[ "$git_status" =~ On\ branch\ ([^[:space:]]+) ]]; then
-            branch=${BASH_REMATCH[1]}
-            test "$branch" != master && branch="[$branch]" || branch=' ☆ '
-        else
-            # Detached HEAD.  (branch=HEAD is a faster alternative.)
-            branch="(`git describe --all --contains --abbrev=4 HEAD 2> /dev/null ||
-                echo HEAD`)"
-        fi
-        echo -n '\[\e[0;'"$ansi"';1m\]'"$branch"'\[\e[0m\] '
-    fi
-}
-function _prompt_command() {
-    PS1="`_git_prompt``marc_prompt`"
-}
 
 function _pre_git_prompt
 {
