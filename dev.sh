cc245cb48db6e092d0edd7f6c19b8ca3805d2f24
Marc Kubischta <mkubisch@adobe.com>
2019-06-25 11:27:39 -0700

    Fix wrapper-test

--- dev.sh
+++ dev.sh
@@ -53,53 +53,73 @@ alias ccx="nvm use v6.10 && pd /git/comments-example"
 
 # sharesheet
 
-#alias yww="if [[ \`ps -A | grep 'sudo yarn watch' | grep -v grep\` ]]; then ps -A -o pid -o command | grep -v grep | grep 'sudo yarn watch' | awk '{print \$1}' | xargs -I PID -t sudo kill PID; fi; sudo yarn watch --hot --disableHostCheck --host 0.0.0.0 0>&- 1>&- 2>&- &"
-
 wrapper-test() {
+  set -e
   # run this from inside your share-sheet-web-page repo root
   # pass in the following args:
   # $1 - github component fork (org/user) name (not remote name)
   # $2 - github component PR branch
+  WRAP_TEST_FORK=$1
+  WRAP_TEST_BRANCH=$2
+
+  START_DIR=`pwd`
 
   # build everything
-  npm ci
+  echo === Building wrapper dependencies from package-lock
+  npm ci || (echo "=== npm ci failed. Is your lock file up to date?" && return 2)
   # remove the artifactory sharesheet component
+  echo === Removing node_modules ccx-share-sheet
   npm uninstall @ccx/ccx-share-sheet
   # install the component from the PR branch to get its dependencies installed
-  npm install --save "git+ssh://git@git.corp.adobe.com:${1}/ccx-share-sheet#${2}"
+  echo === Installing dependencies for ccx-share-sheet from PR branch ${WRAP_TEST_FORK}:${WRAP_TEST_BRANCH}
+  npm install --save "git+ssh://git@git.corp.adobe.com:${WRAP_TEST_FORK}/ccx-share-sheet#${WRAP_TEST_BRANCH}" || (echo === Git branch not found: ${WRAP_TEST_FORK}/ccx-share-sheet#${WRAP_TEST_BRANCH} && return 3)
 
   # remove the sharesheet component module which has no dist folder
-    cd node_modules/@ccx/
-    rm -r ccx-share-sheet
-    
-  # clone the sharesheet component PR source repo so we can build its dist folder
-    git clone "git@git.corp.adobe.com:${1}/ccx-share-sheet"
+  rm -r node_modules/@ccx/ccx-share-sheet
+
+  # wrap this in a method since we are changing dirs and want to be able to cancel cleanly
+  [ -d node_modules/@ccx ] || (echo === ./node_modules/@ccx not found, aborting && return 4)
+  cd node_modules/@ccx/
+  buildComponent() {    
+    set -e
+    # clone the sharesheet component PR source repo so we can build its dist folder
+    echo === Pulling to node_modules ${WRAP_TEST_FORK}/ccx-share-sheet PR branch ${WRAP_TEST_BRANCH} 
+    git clone "git@git.corp.adobe.com:${WRAP_TEST_FORK}/ccx-share-sheet" || (echo === Git clone failed && return 5)
+    [ -d ccx-share-sheet ] || (echo === ./node_modules/@ccx/ccx-share-sheet not found, aborting && return 6)
     cd ccx-share-sheet
+    buildComponentPostPull() {
+      set -e
 
-  # pull and checkout the PR branch
-    git fetch --tags --progress "git@git.corp.adobe.com:${1}/ccx-share-sheet" +refs/pull/*:refs/remotes/origin/pr/*
-    git branch -r | grep ${2}
-    git fetch origin "${2}"
-    git checkout "${2}"
+    # pull and checkout the PR branch
+      git fetch --tags --progress "git@git.corp.adobe.com:${WRAP_TEST_FORK}/ccx-share-sheet" +refs/pull/*:refs/remotes/origin/pr/*
+      git branch -r | grep ${WRAP_TEST_BRANCH}
+      git fetch origin "${WRAP_TEST_BRANCH}"
+      git checkout "${WRAP_TEST_BRANCH}"
 
-  # build node modules and dist folder
-    cp ../../../.npmrc .
-    npm ci
-    npm run build
+    # build node modules and dist folder
+      echo === Building PR branch ccx-share-sheet#${WRAP_TEST_BRANCH} from its package-lock
+      cp ../../../.npmrc .
+      npm ci || (echo "=== npm ci failed in component, aborting" && return 7)
+      npm run build
 
-  # perform the build step that updates the index.js to point to dist
-    sed -i'.original' 's/src/dist/g' index.js
+    # perform the build step that updates the index.js to point to dist
+      sed -i'.original' 's/src/dist/g' index.js
 
-  # clean up
-    rm -r node_modules
-    rm -r src
-  
+    # clean up
+      rm -r node_modules
+      rm -r src
+    }
+    buildComponentPostPull
+  }
+  buildComponent
   # back to wrapper repo
-    cd ../../../
+  cd $START_DIR
 
   # run and zip the wrapper build
+  echo === Building wrapper using PR branch component
   npm run build-production
   zip -r build.zip build -x *.js.map *.DS_Store
+  echo === Build with ${WRAP_TEST_FORK}:${WRAP_TEST_BRANCH} is in ./build/ and ./build.zip
 }
 
 wds-stop-all() {
