b49ff52a74b9e41e38cf296c005f4820e834e20d
Marc Kubischta <mkubisch@adobe.com>
2019-08-13 11:03:22 -0700

    android studio and python updates

--- dev.sh
+++ dev.sh
@@ -1,11 +1,16 @@
 # adb
 declare -x ANDROID_HOME="$HOME/Library/Android/sdk"
+declare -x PATH="$PATH:$ANDROID_HOME/build-tools/29.0.0"
+declare -x PATH="$PATH:$ANDROID_HOME/emulator"
 declare -x PATH="$PATH:$ANDROID_HOME/platform-tools"
 declare -x PATH="$PATH:$ANDROID_HOME/tools"
 
-# GO
-export GOPATH=$HOME/go
-declare -x PATH="$PATH:$GOPATH/bin:/usr/local/opt/go/libexec/bin" # golang exe
+# python
+declare -x PATH="/usr/local/opt/python/libexec/bin:$PATH"
+alias python2='PATH=/usr/local/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin python'
+
+# JAVA
+declare -x JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk1.8.0_66.jdk/Contents/Home"
 
 # cmake
 declare -x CMAKE_HOME="/Applications/CMake.app/Contents/bin"
